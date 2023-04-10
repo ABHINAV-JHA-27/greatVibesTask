6a9c9eb602202ad69c02b95f105eedd3cb19ec4d
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-04-10 23:46:18 +0530

    Added Create JOb Button and Created respective forms and improved css

---
+++ src/components/CreateForm/index.jsx
@@ -0,0 +1,334 @@
+import { useState } from "react";
+
+const CreateForm = ({ visible }) => {
+    const [jobTitle, setJobTitle] = useState("");
+    const [companyName, setCompanyName] = useState("");
+    const [industry, setIndustry] = useState("");
+    const [location, setLocation] = useState("");
+    const [remoteType, setRemoteType] = useState("");
+    const [minExperience, setMinExperience] = useState("");
+    const [maxExperience, setMaxExperience] = useState("");
+    const [minSalary, setMinSalary] = useState("");
+    const [maxSalary, setMaxSalary] = useState("");
+    const [totalEmployees, setTotalEmployees] = useState("");
+    const [applyType, setApplyType] = useState("");
+
+    const [jobTitleError, setJobTitleError] = useState(false);
+    const [companyNameError, setCompanyNameError] = useState(false);
+    const [industryError, setIndustryError] = useState(false);
+
+    const [showForm2, setShowForm2] = useState(false);
+
+    if (!visible) return null;
+    return (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+            {showForm2 ? (
+                <div className="flex bg-white w-[40vw] h-[70vh] border-[1px] border-[#E6E6E6] rounded-[10px] m-2 flex-col relative top-20 mx-auto">
+                    <div className="flex flex-row w-[96%] ml-4 mt-5 items-center">
+                        <div className="flex w-[50%]">
+                            <span className="text-base font-medium text-black font-[poppins]">
+                                Create a Job
+                            </span>
+                        </div>
+                        <div className="flex w-[50%]">
+                            <span className="text-sm font-medium text-black font-[poppins]">
+                                Step 2
+                            </span>
+                        </div>
+                    </div>
+                    <div className="mt-1 flex flex-col">
+                        <div className="mt-4">
+                            <div className="ml-4">
+                                <span className="text-sm font-medium text-black font-[poppins]">
+                                    Experience
+                                </span>
+                            </div>
+                            <div className="mx-4 mt-1 flex flex-row w-[100%] space-x-14">
+                                <input
+                                    placeholder="Minimum"
+                                    value={minExperience}
+                                    onChange={(e) =>
+                                        setMinExperience(e.target.value)
+                                    }
+                                    className="border-[1px] border-[#E6E6E6] rounded-[5px] p-1 w-[42%]"
+                                />
+                                <input
+                                    placeholder="Maximum"
+                                    value={maxExperience}
+                                    onChange={(e) =>
+                                        setMaxExperience(e.target.value)
+                                    }
+                                    className="border-[1px] border-[#E6E6E6] rounded-[5px] p-1 w-[42%]"
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <div className="ml-4">
+                                <span className="text-sm font-medium text-black font-[poppins]">
+                                    Salary
+                                </span>
+                            </div>
+                            <div className="mx-4 mt-1 flex flex-row w-[100%] space-x-14">
+                                <input
+                                    placeholder="Minimum"
+                                    value={minSalary}
+                                    onChange={(e) =>
+                                        setMinSalary(e.target.value)
+                                    }
+                                    className="border-[1px] border-[#E6E6E6] rounded-[5px] p-1 w-[42%]"
+                                />
+                                <input
+                                    placeholder="Maximum"
+                                    value={maxSalary}
+                                    onChange={(e) =>
+                                        setMaxSalary(e.target.value)
+                                    }
+                                    className="border-[1px] border-[#E6E6E6] rounded-[5px] p-1 w-[42%]"
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <div className="ml-4">
+                                <span className="text-sm font-medium text-black font-[poppins]">
+                                    Total Employees
+                                </span>
+                            </div>
+                            <div className="mx-4 mt-1">
+                                <input
+                                    placeholder="ex. 100"
+                                    value={totalEmployees}
+                                    onChange={(e) =>
+                                        setTotalEmployees(e.target.value)
+                                    }
+                                    className="border-[1px] border-[#E6E6E6] rounded-[5px] p-1 w-[100%]"
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <div className="ml-4">
+                                <span className="whitespace-nowrap text-sm font-medium text-black font-[poppins]">
+                                    Apply Type
+                                </span>
+                            </div>
+                            <div className="mt-1 flex flex-row">
+                                <div class="flex items-center mx-4">
+                                    <input
+                                        type="radio"
+                                        value=""
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                    />
+                                    <label class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                        Quick Apply
+                                    </label>
+                                </div>
+                                <div class="flex items-center mx-4">
+                                    <input
+                                        type="radio"
+                                        value=""
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                    />
+                                    <label class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                        External Apply
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row-reverse mx-4 mt-[12vh]">
+                        <div className="bg-[#1597E4] rounded-[5px] p-2 w-[6vw] text-center">
+                            <span className="text-white font-normal font-[poppins] font-medium text-base">
+                                Save
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div
+                    className={
+                        "flex bg-white w-[40vw] h-[70vh] border-[1px] border-[#E6E6E6] rounded-[10px] m-2 flex-col relative top-20 mx-auto"
+                    }
+                >
+                    <div
+                        className={
+                            "flex flex-row w-[96%] ml-4 mt-5 items-center"
+                        }
+                    >
+                        <div className={"flex w-[50%]"}>
+                            <span
+                                className={
+                                    "text-base font-medium text-black font-[poppins]"
+                                }
+                            >
+                                Create a Job
+                            </span>
+                        </div>
+                        <div className={"flex w-[50%]"}>
+                            <span
+                                className={
+                                    "text-sm font-medium text-black font-[poppins]"
+                                }
+                            >
+                                Step 1
+                            </span>
+                        </div>
+                    </div>
+                    <div className={"mt-1 flex flex-col"}>
+                        <div className={"mt-4"}>
+                            <div className={"ml-4"}>
+                                <span
+                                    className={
+                                        "text-sm font-medium text-black font-[poppins]"
+                                    }
+                                >
+                                    Job Title
+                                </span>
+                            </div>
+                            <div className={"mx-4 mt-1"}>
+                                <input
+                                    placeholder="ex. UX UI Designer"
+                                    value={jobTitle}
+                                    onChange={(e) =>
+                                        setJobTitle(e.target.value)
+                                    }
+                                    className={`border-[1px] ${
+                                        jobTitleError
+                                            ? "border-red"
+                                            : "border-[#E6E6E6]"
+                                    } rounded-[5px] p-1 w-[100%]`}
+                                />
+                            </div>
+                        </div>
+                        <div className={"mt-4"}>
+                            <div className={"ml-4"}>
+                                <span
+                                    className={
+                                        "text-sm font-medium text-black font-[poppins]"
+                                    }
+                                >
+                                    Company Name
+                                </span>
+                            </div>
+                            <div className={"mx-4 mt-1"}>
+                                <input
+                                    placeholder="ex. Google"
+                                    value={companyName}
+                                    onChange={(e) =>
+                                        setCompanyName(e.target.value)
+                                    }
+                                    className={`border-[1px] ${
+                                        companyNameError
+                                            ? "border-red"
+                                            : "border-[#E6E6E6]"
+                                    } rounded-[5px] p-1 w-[100%]`}
+                                />
+                            </div>
+                        </div>
+                        <div className={"mt-4"}>
+                            <div className={"ml-4"}>
+                                <span
+                                    className={
+                                        "text-sm font-medium text-black font-[poppins]"
+                                    }
+                                >
+                                    Industry
+                                </span>
+                            </div>
+                            <div className={"mx-4 mt-1"}>
+                                <input
+                                    placeholder="ex. Information Technology"
+                                    value={industry}
+                                    onChange={(e) =>
+                                        setIndustry(e.target.value)
+                                    }
+                                    className={`border-[1px] ${
+                                        industryError
+                                            ? "border-red"
+                                            : "border-[#E6E6E6]"
+                                    } rounded-[5px] p-1 w-[100%]`}
+                                />
+                            </div>
+                        </div>
+                        <div className={"flex flex-row mt-4 mx-4 space-x-14"}>
+                            <div className={"flex flex-col w-[45%]"}>
+                                <div className={""}>
+                                    <span
+                                        className={
+                                            "text-sm font-medium text-black font-[poppins]"
+                                        }
+                                    >
+                                        Location
+                                    </span>
+                                </div>
+                                <div className={"mt-1"}>
+                                    <input
+                                        placeholder="ex. Chennai"
+                                        value={location}
+                                        onChange={(e) =>
+                                            setLocation(e.target.value)
+                                        }
+                                        className={
+                                            "border-[1px] border-[#E6E6E6] rounded-[5px] p-1 w-[100%]"
+                                        }
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-col w-[45%]">
+                                <div className="">
+                                    <span className="whitespace-nowrap text-sm font-medium text-black font-[poppins]">
+                                        Remote Type
+                                    </span>
+                                </div>
+                                <div className="mt-1">
+                                    <input
+                                        placeholder="ex. In-office"
+                                        value={remoteType}
+                                        onChange={(e) =>
+                                            setRemoteType(e.target.value)
+                                        }
+                                        className="border-[1px] border-[#E6E6E6] rounded-[5px] p-1 w-[100%]"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row-reverse mx-4 mt-[12vh]">
+                        <div className="bg-[#1597E4] rounded-[5px] p-2 w-[6vw] text-center">
+                            <span
+                                className="text-white font-normal font-[poppins] font-medium text-base"
+                                onClick={() => {
+                                    if (jobTitle === "") {
+                                        setJobTitleError(true);
+                                    } else {
+                                        setJobTitleError(false);
+                                    }
+                                    if (companyName === "") {
+                                        setCompanyNameError(true);
+                                    } else {
+                                        setCompanyNameError(false);
+                                    }
+                                    if (industry === "") {
+                                        setIndustryError(true);
+                                    } else {
+                                        setIndustryError(false);
+                                    }
+
+                                    if (
+                                        jobTitle !== "" &&
+                                        companyName !== "" &&
+                                        industry !== ""
+                                    ) {
+                                        setShowForm2(true);
+                                    }
+                                }}
+                            >
+                                Next
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CreateForm;
--- src/components/JobCard/index.jsx
+++ src/components/JobCard/index.jsx
@@ -3,42 +3,62 @@ import logo from "../../assets/logo.png";
 
 const JobCard = () => {
     return (
-        <div className="flex bg-white w-[43vw] h-[33vh] border-[1px] border-[#DADEDF] rounded-[10px] m-2">
+        <div className="flex bg-white w-[43vw] h-[34vh] border-[1px] border-[#DADEDF] rounded-[10px] m-2 flex-col">
             <div className="flex flex-row w-[100%] m-2">
-                <div className="flex flex-col w-[12%] border-[1px]">
+                <div className="flex flex-col w-[12%]">
                     <img
                         src={logo}
                         className="w-[3vw] h-[3vw] self-center m-1"
                     />
                 </div>
-                <div className="flex-col w-[88%] border-[1px]">
-                    <span className="text-base font-normal text-black font-[poppins]">
-                        UX UI Designer
-                    </span>
-                    <br />
-                    <span className="text-sm font-normal text-black font-[poppins]">
-                        Great Vibes - Information Technology
-                    </span>
-                    <br />
-                    <span className="text-sm font-normal text-[#646464] font-[poppins]">
-                        Chennai, Tamil Nadu, India (In-office)
-                    </span>
-                    <br />
-                    <span className="text-sm font-normal text-black font-[poppins]">
-                        Part-Time (9.00 am - 5.00 pm IST)
-                    </span>
-                    <br />
-                    <span className="text-sm font-normal text-black font-[poppins]">
-                        Experience (1 - 2 years)
-                    </span>
-                    <br />
-                    <span className="text-sm font-normal text-black font-[poppins]">
-                        INR (₹) 30,000 - 50,000 / Month
-                    </span>
-                    <br />
-                    <span className="text-sm font-normal text-black font-[poppins]">
-                        51-200 employees
-                    </span>
+                <div className="flex-col w-[88%]">
+                    <div>
+                        <span className="text-base font-normal text-black font-[poppins]">
+                            UX UI Designer
+                        </span>
+                    </div>
+                    <div>
+                        <span className="text-sm font-normal text-black font-[poppins]">
+                            Great Vibes - Information Technology
+                        </span>
+                    </div>
+                    <div>
+                        <span className="text-sm font-normal text-[#646464] font-[poppins]">
+                            Chennai, Tamil Nadu, India (In-office)
+                        </span>
+                    </div>
+                    <div className="mt-3">
+                        <span className="text-sm font-normal text-black font-[poppins] ">
+                            Part-Time (9.00 am - 5.00 pm IST)
+                        </span>
+                    </div>
+                    <div className="mt-1">
+                        <span className="text-sm font-normal text-black font-[poppins]">
+                            Experience (1 - 2 years)
+                        </span>
+                    </div>
+                    <div className="mt-1">
+                        <span className="text-sm font-normal text-black font-[poppins]">
+                            INR (₹) 30,000 - 50,000 / Month
+                        </span>
+                    </div>
+                    <div className="mt-1">
+                        <span className="text-sm font-normal text-black font-[poppins]">
+                            51-200 employees
+                        </span>
+                    </div>
+                    <div className="flex flex-row w-[100%] mt-4">
+                        <div className="bg-[#1597E4] rounded-[5px] p-1">
+                            <span className="text-white font-normal font-[poppins] font-medium text-sm">
+                                Apply Now
+                            </span>
+                        </div>
+                        <div className="bg-white border-[1px] border-[#1597E4] rounded-[5px] ml-3 p-1">
+                            <span className="text-[#1597E4] font-normal font-[poppins] font-medium text-sm">
+                                External Apply
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
